f186ae3c6218f50225d58ad3d99b2bd03067a125
Aaditya <aaditya.sp@somaiya.edu>
2023-07-31 09:39:38 +0530

    31/7/23

--- NGT/JQUERY/js/prac1.js
+++ NGT/JQUERY/js/prac1.js
@@ -26,4 +26,15 @@ $('.car_list_tag').hover(function(){
 }
 );
 
+//$("div:not(#porsche)").fadeOut("slow"); //select the divs that dont have id porsche
+
+//$("div:not(.mb-3)").fadeOut("slow"); //select the divs that dont have class mb-3
+
+//$("div.mb-3").fadeOut("slow"); //select all the div with the class mb-3
+
+$("div#list_of_cars.container").css("background-color","red"); //select a div with specified id AND class
+
+$("div.mb-3 select#input_car_brand option").css("color","blue"); //select div with class mb-3 inside which select a  'select' tag with id input_car_brand
+
+$("input[@id=input_add_new_brand]").css("color","green"); 
 })
