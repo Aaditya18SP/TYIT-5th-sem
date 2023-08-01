c3626bb3b9737400f7ef56a54b0745a5c53a10f2
Aaditya <aaditya.sp@somaiya.edu>
2023-08-01 10:31:55 +0530

    added nqueens

---
+++ AI/Practicals/Nqueens.py
@@ -0,0 +1,43 @@
+from math import *
+import sys
+
+x={} # to store which queen is stored at in which column
+
+n=int(4) # number of queens and rows
+
+
+#function to clear the blocks 
+def clear_future_blocks(k):
+    for i in range(k,n+1):
+        x[i]=None
+
+        
+def place(k,i):
+    if(i in x.values()):
+        return False
+    j=1
+    while(j<k):
+        if(abs(x[j])-i==abs(j-k)):
+           return False
+        j+=1
+    return True
+
+def Nqueens(k):
+    for i in range(1,n+1):
+        clear_future_blocks(k)
+        if(place(k,i)):
+            x[k]=i
+            if(k==n):
+                for j in x:
+                    print(x[j],end='')
+                    '''for k in range(1,n+1):
+                        if(k==x[j]):
+                            print('Q',end='')
+                        else:
+                            print('.',end='')
+                            print()'''
+        else:
+            Nqueens(k+1)
+
+Nqueens(1)
+        
