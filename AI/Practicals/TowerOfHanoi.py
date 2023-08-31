d94b3d6f80bfeb52251dd0fc01fd19c073c0acfc
Aaditya18SP <aaditya.sp@somaiya.edu>
2023-09-01 00:38:38 +0530

    new contents added

---
+++ AI/Practicals/TowerOfHanoi.py
@@ -0,0 +1,10 @@
+def moveTower(height, frompole, topole, withpole):
+    if height>0:
+        moveTower(height-1,frompole,withpole,topole)
+        moveDisk(frompole,topole)
+        moveTower(height-1, withpole,topole,frompole)
+
+def moveDisk(fp,tp):
+    print("moving disk from ",fp ,"to ",tp)
+
+moveTower(3,"A","B","C")
