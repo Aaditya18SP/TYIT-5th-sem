ab75e387da8738cd19e6d126f7dce02fd8e70a7c
Aaditya <aaditya.sp@somaiya.edu>
2023-09-26 10:29:05 +0530

    26/9/23

---
+++ AWP/Practicals/prac6_website/Default.aspx
@@ -0,0 +1,131 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+    <div class="div_display_all_students">
+     
+    <asp:ListBox runat="server" ID="lboxAllDetails" Width="100%" 
+            onselectedindexchanged="lboxAllDetails_SelectedIndexChanged" AutoPostBack="true" />
+
+    <!--DataSourceID="SqlDataSource1" 
+            DataTextField="ctrl_id" DataValueField="ctrl_id"
+    
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" 
+            ConnectionString="<%$ ConnectionStrings:masterConnectionString %>" 
+            SelectCommand="SELECT * FROM [students]"></asp:SqlDataSource>
+
+            -->
+    </div>
+
+    <div class="div_display_specific_details">
+    <h1> The Details of the user</h1>
+    <!--CTRL ID-->
+    <asp:Label runat="server" ID="lbforDispCtrlId" Text="Control id:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbDispCtrlId" Enabled="false" />
+    <br />
+    <br />
+
+
+     <!--NAME-->
+    <asp:Label runat="server" ID="lbforDispName" Text="Name:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbDispName" Enabled="false"/>
+    <br />
+    <br />
+
+     <!--EMAIL-->
+    <asp:Label runat="server" ID="lbforDispEmail" Text="Email:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbDispEmail" Enabled="false" />
+    <br />
+    <br />
+
+     <!--PHONE-->
+    <asp:Label runat="server" ID="lbforDispPhone" Text="Phone:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbDispPhone" Enabled="false"/>
+    <br />
+
+     <!--Address-->
+    <asp:Label runat="server" ID="lbforDispAddress" Text="Address:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbDispAddress" TextMode="multiline"  Width="50%" Enabled="false" />
+    <br />
+    <br />
+
+    <!--Edit Button-->
+     <asp:Button runat="server" ID="btnEdit" Text="Edit" onclick="btnEdit_Click"/>
+
+    <!--Save Button-->
+    <asp:Button runat="server" ID="btnSave" Text="Save" UseSubmitBehavior="true" 
+            style="margin-left:20px" visible="false" onclick="btnSave_Click"/>
+
+    <!--Delete Button-->
+    <asp:Button runat="server" ID="btnDelete" Text="Delete"
+            style="margin-left:20px" onclick="btnDelete_Click"/>
+    </div>
+
+    <!--
+    A single textbox to display all the details
+    <div class="div_display_specific_details">
+
+    <h1>Displaying details of user</h1>
+
+    <asp:TextBox runat="server" TextMode="multiline" id="tbDisplayDetails"></asp:TextBox>
+    
+    </div>
+
+    -->
+
+    <div class="input_user_details">
+    <h1> Enter Details to insert</h1>
+    <!--CTRL ID-->
+    <asp:Label runat="server" ID="lbCtrlId" Text="Control id:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbCtrlId" />
+    <br />
+    <br />
+
+
+     <!--NAME-->
+    <asp:Label runat="server" ID="lbForName" Text="Name:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbName"/>
+    <br />
+    <br />
+
+     <!--EMAIL-->
+    <asp:Label runat="server" ID="lbForEmail" Text="Email:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbEmail" />
+    <br />
+    <br />
+
+     <!--PHONE-->
+    <asp:Label runat="server" ID="lbForPhone" Text="Phone:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbPhone" />
+    <br />
+
+     <!--Address-->
+    <asp:Label runat="server" ID="lbForAddress" Text="Address:"/>
+    <br />
+    <asp:TextBox runat="server" ID="tbAddress" TextMode="multiline" Width="50%" />
+    <br />
+  
+    <br />
+
+    <!--Insert Button-->
+    <asp:Button runat="server" ID="btnInsert" Text="Insert" onclick="btnInsert_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ AWP/Practicals/prac6_website/Default.aspx.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+//these are newly added 
+using System.Data.SqlClient;
+using System.Data;
+using System.Text; //for string builder
+
+public partial class _Default : System.Web.UI.Page
+{
+    //Create a connection to the database
+    SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=master;Integrated Security=True");
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //load the data into the listbox programmatically
+        //SqlConnection conn = new SqlConnection("Data Source=.;Initial Catalog=master;Integrated Security=True");
+
+        string fetchCtrlIDQuery = "select * from students";
+
+        SqlCommand cmd = new SqlCommand(fetchCtrlIDQuery,conn);
+
+        SqlDataReader reader;
+
+        try
+        {
+            //open the connection first
+            conn.Open(); 
+
+            //use the sqlcommand object to execute the query and store in reader objet.
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                ListItem lt = new ListItem();
+                lt.Value = reader["control_id"].ToString();
+                lt.Text = reader["control_id"].ToString();
+
+                lboxAllDetails.Items.Add(lt);
+ 
+            }
+            reader.Close();
+        }
+        catch (Exception err)
+        {
+            tbDisplayDetails.Text = "Error Occurred. It says: " + err.Message;
+        }
+        finally
+        {
+            conn.Close();
+        }
+
+
+    }
+
+    //-------------WHEN LISTBOX ITEMS ARE SELECTED------------------------------
+
+    protected void lboxAllDetails_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string selected_ctrl_id = lboxAllDetails.SelectedItem.Value.ToString();
+       // tbDisplayDetails.Text = "here";
+        string fetchAllDetailsQuery = "select * from students where control_id = @ctrl_id";
+        
+        SqlCommand cmd = new SqlCommand(fetchAllDetailsQuery, conn);
+
+        cmd.Parameters.AddWithValue("@ctrl_id", selected_ctrl_id);
+
+        SqlDataReader reader;
+        try
+        {
+            conn.Open();
+
+            reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                //-------------------------DISPLAY DETAILS IN DIFFERENT TEXTBOXES--------------------------
+                tbDispCtrlId.Text = reader["control_id"].ToString();
+                tbDispName.Text = reader["name"].ToString();
+                tbDispEmail.Text = reader["email"].ToString();
+                tbDispPhone.Text = reader["contactno"].ToString();
+                tbDispAddress.Text = reader["address"].ToString();
+
+
+                //--------------------DISPLAY DETAILS IN A SINGLE TEXTBOX------------------------
+               /* string final_output = "Control id:" + reader["control_id"].ToString() + "\n" +
+                    "Name:" + reader["name"].ToString() + "\n" +
+                    "Email:" + reader["email"].ToString() + "\n" +
+                    "Contact no:" + reader["contactno"].ToString() + "\n" +
+                    "Address:" + reader["address"].ToString() + "\n";
+                */
+
+                //tbDisplayDetails.Text = final_output;
+                break; //to execute the query only once break out of the while loop
+            }
+            reader.Close();
+        }
+        catch (Exception err)
+        {
+            tbDisplayDetails.Text = "Error Occurred. It says:\n" + err.Message;
+        }
+        finally
+        {
+            conn.Close();
+        }
+    }
+
+
+    //---------------------------WHEN EDIT BUTTON IS CLICKED-------------
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+        
+        tbDispCtrlId.Enabled = true;
+        tbDispName.Enabled = true;
+        tbDispEmail.Enabled = true;
+        tbDispPhone.Enabled = true;
+        tbDispAddress.Enabled = true;
+
+        btnSave.Visible = true;
+
+        btnEdit.Visible = false;
+    }
+
+    //-------------------------WHEN THE SAVE BUTTON IS CLICKED---------------
+    protected void btnSave_Click(object sender, EventArgs e)
+    {
+        btnEdit.Visible = true;
+        btnSave.Visible = false;
+
+        tbDispCtrlId.Enabled = false;
+        tbDispName.Enabled = false;
+        tbDispEmail.Enabled = false;
+        tbDispPhone.Enabled = false;
+        tbDispAddress.Enabled = false;
+
+
+        /*--------------
+         PERFORM UPDATE
+         --------------*/
+
+
+
+    }
+
+    //--------------------WHEN DELETE BUTTON IS CLICKED-------------------
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    //----------------------WHEN INSERT BUTTON IS CLICKED--------------------
+    protected void btnInsert_Click(object sender, EventArgs e)
+    {
+        string ctrl_id = tbCtrlId.Text;
+        string name = tbName.Text;
+        string email = tbEmail.Text;
+        string contactno = tbPhone.Text;
+        string address = tbAddress.Text;
+
+        string insertQuery = "insert into students";
+
+    }
+}
